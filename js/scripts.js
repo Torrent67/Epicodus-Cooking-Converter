716622858e4f5fefef2c2eb1387ada96000ef54e
Marqis Ramos and Trevor Teigen <marqis@>
2019-06-05 14:48:46 -0700

    setup design, HTML, and some backend code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
+$(document).ready(function(){
+  
+});
 
-var unit= prompt ("Enter quart or cup");
-var amount = prompt("Enter amount");
+// if (unit=="quart") {
+// var gal = (amount/4) + " gallons";
+//   alert (gal);
+// } else if (unit=="cup"){
+//   var floz = (amount*8)+" fluid ounces";
+//   alert (floz)
+// } else {
+//   alert ("You did not indicate 'quart' or 'cup'.")
+// }
 
-if (unit=="quart") {
-var gal = (amount/4) + " gallons";
-  alert (gal);
-} else if (unit=="cup"){
-  var floz = (amount*8)+" fluid ounces";
-  alert (floz)
-} else {
-  alert ("You did not indicate 'quart' or 'cup'.")
-}
+var firstliquid = parseInt($(".panelLiquids#unit1").val());
+var secondLiquid = parseInt($(".panelLiquids#unit2").val());
+
+var conversion= function(firstLiquid, secondLiquid) {
+  if (firstLiquid==1,secondLiquid==2) {
+    return firstLiquid/8;
+  } else if (firstLiquid==1,secondLiquid==3) {
+    return firstLiquid/32
+  } else if (firstLiquid==1,secondLiquid==4) {
+    return firstLiquid/128;
+  }
+})
